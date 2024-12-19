2b06ff2117ab0d2a28530eccec294425595e56cc
Lorenzo <lorenzofumi@gmail.com>
2024-12-19 19:24:40 +0100

    messages: added 2 sim controller commands

--- src/message.rs
+++ src/message.rs
@@ -91,11 +91,16 @@ pub enum ServerType {
     Media,
 }
 
+/**
+ * Command that can be sent from the simulation controller to the clients
+ */
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum SimControllerCommand {
-    SendMessage(String, NodeId), // Send message to a server
+    SendMessage(String, NodeId, NodeId), // Send message to a server, the first id is the server, the second the destination client
     Register(NodeId),            // Register a client to a server
-    ClientList,                  // Get the list of available clients
+    ClientList(NodeId),          // Get the list of available clients from a server
+    FloodRequest,                // Send a flood request
+    Topology,                    // Get the topology of the network
 }
 
 impl DroneSend for SimControllerCommand {}
