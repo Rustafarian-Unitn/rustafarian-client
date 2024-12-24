4238c9c9ee94e7341d778ebb5f9bc3ac9a7df6d7
Lorenzo <lorenzofumi@gmail.com>
2024-12-24 11:51:55 +0100

    feat(util): build browser client

--- src/tests/util.rs
+++ src/tests/util.rs
@@ -1,12 +1,22 @@
 use std::collections::HashMap;
 
 use crossbeam_channel::{unbounded, Receiver, Sender};
-use rustafarian_shared::messages::commander_messages::{SimControllerCommand, SimControllerResponseWrapper};
+use rustafarian_shared::messages::commander_messages::{
+    SimControllerCommand, SimControllerResponseWrapper,
+};
 use wg_2024::packet::Packet;
 
-use crate::{chat_client::ChatClient, client::Client};
+use crate::{browser_client::BrowserClient, chat_client::ChatClient, client::Client};
 
-pub(crate) fn build_client() -> (ChatClient, (Sender<Packet>, Receiver<Packet>), (Sender<SimControllerCommand>, Receiver<SimControllerCommand>), (Sender<SimControllerResponseWrapper>, Receiver<SimControllerResponseWrapper>)) {
+pub(crate) fn build_client() -> (
+    ChatClient,
+    (Sender<Packet>, Receiver<Packet>),
+    (Sender<SimControllerCommand>, Receiver<SimControllerCommand>),
+    (
+        Sender<SimControllerResponseWrapper>,
+        Receiver<SimControllerResponseWrapper>,
+    ),
+) {
     let neighbor: (Sender<Packet>, Receiver<Packet>) = unbounded();
     let mut neighbors = HashMap::new();
     neighbors.insert(2 as u8, neighbor.0.clone());
@@ -29,5 +39,49 @@ pub(crate) fn build_client() -> (ChatClient, (Sender<Packet>, Receiver<Packet>),
     chat_client.topology().add_edge(2, 21);
     chat_client.topology().add_edge(1, 2);
 
-    (chat_client, neighbor, controller_channel_commands, controller_channel_messages)
+    (
+        chat_client,
+        neighbor,
+        controller_channel_commands,
+        controller_channel_messages,
+    )
+}
+
+pub(crate) fn build_browser() -> (
+    BrowserClient,
+    (Sender<Packet>, Receiver<Packet>),
+    (Sender<SimControllerCommand>, Receiver<SimControllerCommand>),
+    (
+        Sender<SimControllerResponseWrapper>,
+        Receiver<SimControllerResponseWrapper>,
+    ),
+) {
+    let neighbor: (Sender<Packet>, Receiver<Packet>) = unbounded();
+    let mut neighbors = HashMap::new();
+    neighbors.insert(2 as u8, neighbor.0.clone());
+    let channel: (Sender<Packet>, Receiver<Packet>) = unbounded();
+    let client_id = 1;
+
+    let controller_channel_commands = unbounded();
+    let controller_channel_messages = unbounded();
+
+    let mut browser_client = BrowserClient::new(
+        client_id,
+        neighbors,
+        channel.1,
+        controller_channel_commands.1.clone(),
+        controller_channel_messages.0.clone(),
+    );
+
+    browser_client.topology().add_node(2);
+    browser_client.topology().add_node(21);
+    browser_client.topology().add_edge(2, 21);
+    browser_client.topology().add_edge(1, 2);
+
+    (
+        browser_client,
+        neighbor,
+        controller_channel_commands,
+        controller_channel_messages,
+    )
 }
